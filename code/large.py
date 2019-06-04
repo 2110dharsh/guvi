269b8235bbd02efae80134da1fdd10eb71d42c40
2110dharsh <51355727+2110dharsh@users.noreply.github.com>
2019-06-04 16:20:21 +0530

    Update large.py

--- code/large.py
+++ code/large.py
@@ -1,11 +1,10 @@
-num1=int(input())
-num2=int(input())
-num3=int(input())
-if((num1 >= num2) and (num1 >=num3)):
-Output=num1
-elif((num2>=num1) and (num2 >= num3)):
-Output=num2
+num1 = int(input())
+num2== int(input())
+num3 = int(input())
+if (num1 >= num2) and (num1 >= num3):
+   Output = num1
+elif (num2 >= num1) and (num2 >= num3):
+   Output= num2
 else:
-Output=num3
+   Output = num3
 print(Output)
- 
