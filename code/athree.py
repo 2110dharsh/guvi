c8789acb2af8f6f919abef103852194a10809362
2110dharsh <51355727+2110dharsh@users.noreply.github.com>
2019-06-04 14:20:51 +0530

    Create athree.py

---
+++ code/athree.py
@@ -0,0 +1,9 @@
+ch = input()
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+       print("Vowel")
+elif(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+       print("Consonant")
+else:
+    print("Invalid")
