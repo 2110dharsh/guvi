5e8e8b792a3b0d743c9d106f4fb6b86ff10baf4a
2110dharsh <51355727+2110dharsh@users.noreply.github.com>
2019-07-26 12:59:37 +0530

    Create pro22c.py

---
+++ code/pro22c.py
@@ -0,0 +1,16 @@
+#a
+counter=int(input(" "))
+arr=list(map(int,input().split()))
+arr.sort(reverse=True)
+#print(arr)
+sum=0
+sum1=0
+result=[]
+for i in range(0,counter,2):
+    sum=sum+arr[i]
+for j in range(1,counter,2):
+    sum1=sum1+arr[j]
+result.append([sum,sum1])
+#print(result)
+for i in result:
+    print(i[0] if i[0]>i[1] else i[1])
