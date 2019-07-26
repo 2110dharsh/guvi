aded8dca351e1d6df826b6ef88bb919756300041
2110dharsh <51355727+2110dharsh@users.noreply.github.com>
2019-07-26 12:21:10 +0530

    Update pro21.py

--- code/pro21.py
+++ code/pro21.py
@@ -1,13 +1,13 @@
 import statistics as st
-	counter=int(input(" "))
-	arr=list(map(int,input().split()))
-	result=False
-	for i in range(1,counter):
-	    l1=arr[:i]
-	    l2=arr[i:]
-	    if st.mean(l1)==st.mean(l2):
-	        result=True
-	if result==True:
-	    print("yes")
-	else:
-	    print("no")
+counter=int(input(" "))
+arr=list(map(int,input().split()))
+result=False
+for i in range(1,counter):
+    l1=arr[:i]
+    l2=arr[i:]
+    if st.mean(l1)==st.mean(l2):
+        result=True
+if result==True:
+    print("yes")
+else:
+    print("no")
